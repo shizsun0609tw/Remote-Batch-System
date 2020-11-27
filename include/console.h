efa2b0c4e70ce484f0aab85122480465e7443d40
shizsun0609tw@gmail.com <zhchen0609@nplinux2.cc.cs.nctu.edu.tw>
2020-11-27 13:34:05 +0800

    update console

--- include/console.h
+++ include/console.h
@@ -1,5 +1,21 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 
+class Console
+{
+public:
+	Console();
+	
+public:
+	void Link2Server();
+	void Run();
+
+private:
+	void SendInitialHTML();
+	void SendShellOutput();
+	void GetShellInput();
+	void GetShellOutput();
+};
 
--- src/console.cpp
+++ src/console.cpp
@@ -1,6 +1,53 @@
 #include "console.h"
 
+Console::Console()
+{
+
+}
+
+void Console::Link2Server()
+{
+
+}
+
+void Console::Run()
+{
+
+}
+
+void Console::SendInitialHTML()
+{
+	std::string contentType;
+	std::string initHtml;
+
+	contentType = "Content-type: text/html\r\n\r\n";
+	initHTML =
+
+}
+
+void Console::SendShellOutput()
+{
+
+}
+
+void Console::GetShellInput()
+{
+
+}
+
+void Console::GetShellOutput()
+{
+
+}
+
+
 int main(int argc, char* argv[])
 {
+	Console myConsole;
+
+	myConsole.Link2Server();
+
+	myConsole.Run();
+
 	return 0;
 }
